850660c10fb980a44ec438c50b7d4456b0ca7df3
rahul-mitra <rahul1991mira@gmail.com>
2021-05-08 21:56:11 +0530

    minor fixes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ChatUI
+# MomentUi
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.8.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ChatUI'),
+      dir: require('path').join(__dirname, './coverage/Moment'),
       subdir: '.',
       reporters: [
         { type: 'html' },
